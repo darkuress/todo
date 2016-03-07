d5e0dc9be74ec28f76911821f0444c0d3e858a3a
darkuress <jhwang.ani@gmail.com>
2016-03-07 14:55:39 -0800

    adsfasdf

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    templateData = {}
+    return render_template('todo.html', **templateData)
 
+@app.route('/updateData')
+def updateData():
+    try:
+        return str(request.args['data'])
+    except KeyError:
+        return "Missing parameter"
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port = 5002)
