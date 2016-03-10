e6b1a776a1b678a0f23fe0e98074f75fe877dc3e
darkuress <jhwang.ani@gmail.com>
2016-03-10 14:10:38 -0800

    adsfadsf

--- app.py
+++ app.py
@@ -13,8 +13,13 @@ def updateData():
     print 'request....', request
     #-asdfasdf
     data = request.form
+    writingJson(data)
     print 'data is.....', str(data['content'])
     return render_template('todo.html')
-    
+
+def writingJson(data):
+    with open('data.txt', 'w') as outfile:
+        json.dump(data, outfile)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port = 5002)
