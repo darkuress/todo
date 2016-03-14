528f6798c4ea04f5802c0fabaec86f8c8e8926a6
darkuress <jhwang.ani@gmail.com>
2016-03-14 11:18:42 -0700

    waefawef

--- app.py
+++ app.py
@@ -41,8 +41,8 @@ def updateData():
         
     elif parsed_data['action'] == 'delete':
         for one_temp_data in templateData['templateData']:
-            print '............', eval(parsed_data['what'])
-            if one_temp_data['chkbx'] in eval(parsed_data['what']):
+            print '............', eval(str(parsed_data['what']))
+            if one_temp_data['chkbx'] in eval(str(parsed_data['what'])):
                 templateData['templateData'].remove(one_temp_data)
     
     writingJson(templateData)
